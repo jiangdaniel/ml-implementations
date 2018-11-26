255efa86c5ba0c086eee9a69b788446d0bc0281f
Daniel Jiang <daniel.lu.jiang@gmail.com>
2018-11-25 20:16:14 -0800

    Add plotting code

--- sparse/identity.py
+++ sparse/identity.py
@@ -9,13 +9,20 @@ import argparse
 
 import numpy as np
 import torch as th
+from torch.distributions.multivariate_normal import MultivariateNormal
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from tensorboardX import SummaryWriter
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 
 import utils
 from sparse_layer import SparseLayer
+
+
+MARGIN = 0.1
 
 
 def identity(args):
@@ -24,9 +31,18 @@ def identity(args):
     writer = SummaryWriter(os.path.join("./out", args.dir))
 
     for n, (a_local, a_global) in zip(ns, a):
+        if not os.path.exists("./images/{}/".format(args.dir):
+            os.makedirs("./images/{}/".format(args.dir))
+
+        cov = th.eye(n)
+        if args.correlated:
+            for i in range(n-1):
+                cov[i, i+1] = 0.5
+                cov[i+1, i] = 0.5
+        normal = MultivariateNormal(th.zeros(n), cov)
         layer = SparseLayer(n, n, n, a_local, a_global)
         optimizer = optim.Adam(layer.parameters(), args.lr)
-        val = th.randn(args.batch_size, n)
+        val = normal.sample(th.Size([args.batch_size]))
 
         running_loss = 0.
         for epoch in range(args.epochs):
@@ -42,16 +58,18 @@ def identity(args):
                     writer.add_scalar(f"train/{n}dims", running_loss / args.log_iter, epoch)
                     writer.add_scalar(f"val/{n}dims", F.mse_loss(layer(val), val), epoch)
                     running_loss = 0.
-                    means = (layer.D.sigmoid() * layer.shape).detach().cpu().numpy()
-                    sigmas = (F.softplus(layer.sigma + layer.sigma_boost) * n * 0.1 + layer.tau).detach().cpu().numpy()
-                    plot(means, sigmas, n)
+                    means = (layer.D.sigmoid() * layer.shape).unsqueeze(0)
+                    sigmas = (F.softplus(layer.sigma + layer.sigma_boost) * n * 0.1 + layer.tau).unsqueeze(0).unsqueeze(2).repeat((1, 1, 2))
+                    values = layer.v.unsqueeze(0)
 
+                    plt.figure(figsize=(7, 7))
+                    plt.cla()
+                    utils.plot(means, sigmas, values, shape=(n, n))
+                    plt.xlim((-MARGIN*(n-1), (n-1) * (1.0+MARGIN)))
+                    plt.ylim((-MARGIN*(n-1), (n-1) * (1.0+MARGIN)))
 
-def plot(means, sigmas, n):
-    viz = np.zeros((n, n))
-    indices = np.clip(np.round(means), 0, n-1).astype(int)
-    viz[indices[:, 0], indices[:, 1]] = 1
-    print(np.round(viz, 1))
+                    plt.savefig("./images/{}/means{:06}.pdf".format(args.dir, epoch))
+                    plt.close()
 
 
 if __name__ == "__main__":
@@ -59,8 +77,9 @@ if __name__ == "__main__":
     parser.add_argument("--lr", type=float, default=0.005)
     parser.add_argument("--epochs", type=int, default=10000)
     parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("--log-iter", type=int, default=20)
+    parser.add_argument("--log-iter", type=int, default=100)
     parser.add_argument("--dir", default=utils.timestamp())
+    parser.add_argument("--correlated", action="store_true", default=False)
 
     parser.add_argument("--local", type=int, default=2)
     parser.add_argument("--glob", type=int, default=2)
--- sparse/utils.py
+++ sparse/utils.py
@@ -4,6 +4,66 @@ from __future__ import print_function
 
 import datetime
 
+import numpy as np
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
 
 def timestamp():
     return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
+def plot(means, sigmas, values, shape=None, axes=None, flip_y=None, alpha_global=1.0):
+    """
+    From https://github.com/MaestroGraph/sparse-hyper/
+    :param means:
+    :param sigmas:
+    :param values:
+    :param shape:
+    :param axes:
+    :param flip_y: If not None, interpreted as the max y value. y values in the scatterplot are
+            flipped so that the max is equal to zero and vice versa.
+    :return:
+    """
+
+    b, n, d = means.size()
+
+    means = means.data[0, :,:].cpu().numpy()
+    sigmas = sigmas.data[0, :].cpu().numpy()
+    values = values.tanh().data[0, :].cpu().numpy()
+
+    if flip_y is not None:
+        means[:, 0] = flip_y - means[:, 0]
+
+    norm = mpl.colors.Normalize(vmin=-1.0, vmax=1.0)
+    cmap = mpl.cm.RdYlBu
+    map = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+
+    if axes is None:
+        axes = plt.gca()
+
+    colors = []
+    for i in range(n):
+        color = map.to_rgba(values[i])
+
+        alpha = min(0.8, max(0.05, ((sigmas[i, 0] * sigmas[i, 0])+1.0)**-2)) * alpha_global
+        axes.add_patch(Ellipse((means[i, 1], means[i, 0]), width=sigmas[i,1], height=sigmas[i,0], color=color, alpha=alpha, linewidth=0))
+        colors.append(color)
+
+    axes.scatter(means[:, 1], means[:, 0], s=5, c=colors, zorder=100, linewidth=0, edgecolor='k', alpha=alpha_global)
+
+    if shape is not None:
+
+        m = max(shape)
+        step = 1 if m < 100 else m//25
+
+        # gray points for the integer index tuples
+        x, y = np.mgrid[0:shape[0]:step, 0:shape[1]:step]
+        axes.scatter(x.ravel(),  y.ravel(), c='k', s=5, marker='D', zorder=-100, linewidth=0, alpha=0.1* alpha_global)
+
+    axes.spines['right'].set_visible(False)
+    axes.spines['top'].set_visible(False)
+    axes.spines['bottom'].set_visible(False)
+    axes.spines['left'].set_visible(False)
