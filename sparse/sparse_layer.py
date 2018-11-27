465b98ec469e63e807e4c9fe2f56e5260668a886
Daniel Jiang <daniel.lu.jiang@gmail.com>
2018-11-26 22:22:58 -0800

    Remove unused code

--- sparse/sparse_layer.py
+++ sparse/sparse_layer.py
@@ -4,8 +4,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import argparse
-
 import numpy as np
 import torch as th
 import torch.nn as nn
@@ -87,73 +85,3 @@ class SparseLayer(nn.Module):
         probs_intermediate = probs / probs.sum(0, keepdim=True)
         v_prime = (probs_intermediate * self.v).sum(1)
         return D_prime.long(), v_prime
-
-
-def main(args):
-    x = th.rand(50, 100)
-    dim = th.tensor([100., 10.])
-    D = th.rand(args.k, 2)
-    sigma = th.zeros(args.k).uniform_(-1, 1)
-    v = th.zeros(args.k).uniform_(-1, 1)
-    l_1 = l_2 = np.log(x.shape[-1])
-    local_dim = th.tensor([l_1, l_2]).float()
-
-    D.requires_grad_()
-    sigma.requires_grad_()
-    v.requires_grad_()
-
-    D_scaled = D.sigmoid() * dim
-
-    with th.no_grad():
-        D_prime = th.zeros(args.k * (4 + args.local + args.glob), 2)
-
-        select_nearest = th.tensor([[1., 1.], [0., 1.], [1., 0.], [1., 1.]]).repeat(args.k, 1)
-        select_local = (th.rand(args.local * args.k, 2) - 0.5) * local_dim
-        D_prime[:4 * args.k] = D_scaled.repeat(1, 4).view(-1, 2) + select_nearest
-        D_prime[4 * args.k:(4 + args.local) * args.k] = D_scaled.repeat(1, args.local).view(-1, 2) + select_local
-        D_prime[(4 + args.local) * args.k:(4 + args.local + args.glob) * args.k] = th.rand(args.glob * args.k, 2) * dim
-
-        D_prime.round_()
-        D_prime[:, 0].clamp_(0, dim[0]-1)
-        D_prime[:, 1].clamp_(0, dim[1]-1)
-
-    sigma_scaled = F.softplus(sigma + 2).unsqueeze(-1).repeat(1, 2) * dim * 0.1 + args.tau
-    means = D_scaled.t().unsqueeze(0).repeat(args.k * (4 + args.local + args.glob), 1, 1)
-    stds = sigma_scaled.sqrt().t().unsqueeze(0).repeat(args.k * (4 + args.local + args.glob), 1, 1)
-    z = (D_prime.unsqueeze(-1).repeat(1, 1, 3) - means) / stds
-
-    m = normal.Normal(th.tensor([0.0]), th.tensor([1.0]))
-    probs = m.log_prob(z).sum(1).exp()
-
-    # Remove duplicates using Cantor technique described in paper
-    with th.no_grad():
-        cantor = D_prime.sum(1) * (D_prime.sum(1) + 1) / 2 + D_prime[:, 1]
-        cantor_sort, cantor_indices = cantor.sort()
-        mask = th.cat((th.zeros(1, dtype=th.uint8), cantor_sort[1:] == cantor_sort[:-1]))[cantor_indices]
-    probs = probs * (1-mask).unsqueeze(-1).float()
-    probs_intermediate = probs / probs.sum(0, keepdim=True)
-    v_prime = (probs_intermediate * v).sum(1)
-    out = mm(x, D_prime.long(), v_prime, th.Size([100, 10]))
-    loss = out.sum()
-    loss.backward()
-
-
-def mm(input, indices, values, shape):
-    output = th.zeros(input.shape[0], shape[1])
-    for (r, c), val in zip(indices, values):
-        output[:, c] += val * input[:, r]
-    return output
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--epochs", type=int, default=10)
-    parser.add_argument("--batch_size", type=int, default=2)
-
-    parser.add_argument("--k", type=int, default=3)
-    parser.add_argument("--local", type=int, default=2)
-    parser.add_argument("--glob", type=int, default=2)
-    parser.add_argument("--tau", type=float, default=0.1)
-    args = parser.parse_args()
-    main(args)
